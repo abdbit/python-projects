404a9b7741240269ad8d465bcd72cc52c7610018
Abdullah Khan <mabdullahkhan2908@gmail.com>
2025-03-27 16:55:00 +0500

    added total and compleated player work

--- diceGame.py
+++ diceGame.py
@@ -1,13 +1,16 @@
 import random
 
-thirty = 30
 possibleoutcome = ['1','2','3','4','5','6']
 randompick = int(random.choice(possibleoutcome))
 
-while randompick < thirty:
+while playerturns < 30:
    oneplayer = input("Should I roll? (yes/no): ")
    if oneplayer.lower() == "yes":
         randompick = int(random.choice(possibleoutcome))
-        print(randompick)
+        print("Your roll: " + randompick)
+        playerturns = int( 0 + randompick)
+        print("Your score: " + playerturns)
    else:
-       print("sorry")
+       print("Check the spell")
+
+
