b99ba0cbef8aa5d00b423bca5b2f7dbe9b6d1136
Abdullah Khan <mabdullahkhan2908@gmail.com>
2025-03-28 22:58:21 +0500

    game compleated succesfully

--- diceGame.py
+++ diceGame.py
@@ -1,14 +1,50 @@
 import random
+import os 
 
-possibleoutcome = ['1','2','3','4','5','6']
+possibleOutcomes = ['1','2','3','4','5','6']
+maxScore = 300
+totalTurns = 30
+currentTurns = 0
 
-oneplayer = input("Wanna roll the dice? (yes/no): ")   
-if oneplayer.lower() == "yes" or "Yes" or "YES":
-     randompick = int(random.choice(possibleoutcome))
-     print("Your roll: " + str(randompick))
-     playerturns = randompick
-     print("Your score: " + str(playerturns + randompick))
+playerScore = 0
+botScore = 0
+
+while(currentTurns < totalTurns):
+    print("\nTurn number: " + str(currentTurns + 1))
+    promptResponse = input("Wanna roll the dice? (yes/no): ")
+
+    #All options: y, Y, yes, Yes, YES
+    if str.find("yYyesYesYES", promptResponse.lower()) != -1:
+        diceRollPlayer = int(random.choice(possibleOutcomes))
+        diceRollBot = int(random.choice(possibleOutcomes))
+
+        # os.system("cls || clear")
+
+        print("Your roll: " + str(diceRollPlayer), end=" | ")
+        print("My roll: " + str(diceRollBot))
+        
+        playerScore = playerScore + diceRollPlayer
+        botScore = botScore + diceRollBot
+        
+        print("Your score: " + str(playerScore), end=" | ")
+        print("My score: " + str(botScore))
+
+        if playerScore >= maxScore or botScore >= maxScore:
+          if playerScore > botScore:
+              print("You won!")
+          elif botScore > playerScore:
+               print("Hahaha I won!")
+          else:
+              print("It's a draw :(")
+          break
+
+        currentTurns = currentTurns + 1
+    else:
+        print("Please enter only 'y' or 'Y' or 'yes' or 'Yes' or 'YES'")
+
+if playerScore > botScore:
+    print("You won!")
+elif botScore > playerScore:
+    print("Hahaha I won!")
 else:
-     print("Check the spell")
-
-
+    print("It's a draw :(")
